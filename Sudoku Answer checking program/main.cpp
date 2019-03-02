b7f46e823cefae44b1f615047bd9829abcce6dd3
Hongjun Yun <andylang8445@gmail.com>
2019-03-02 01:38:39 -0500

    Added the vertical and horizontal line test using the basic sort algorithm.

--- Sudoku Answer checking program/main.cpp
+++ Sudoku Answer checking program/main.cpp
@@ -6,10 +6,57 @@
 //  Copyright © 2019 HongjunYun. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
+#define N 9
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int cpy[N];
+int map[N][N];
+int line_test();
+int box_test();
+
+int main()
+{
+    int vertical_error=0;
+    int horizontal_error=0;
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            scanf("%d",&map[i][j]);
+            cpy[j]=map[i][j];
+        }
+        vertical_error+=line_test();
+    }
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            cpy[j]=map[j][i];
+        }
+        horizontal_error+=line_test();
+    }
+}
+
+int line_test()
+{
+    for(int i=0;i<N-1;i++)
+    {
+        for(int j=i+1;j<N;j++)
+        {
+            if(cpy[i]==cpy[j])
+            {
+                return 1;
+            }
+            else if(cpy[i]>cpy[j])
+            {
+                int tmp=cpy[i];
+                cpy[i]=cpy[j];
+                cpy[j]=tmp;
+            }
+        }
+        for(int j=0;j<N;j++)
+            if(cpy[j]!=j+1)
+                return 1;
+    }
     return 0;
 }
