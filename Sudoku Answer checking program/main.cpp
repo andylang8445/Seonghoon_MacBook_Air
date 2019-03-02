1d70c620a62c902c13ad93c9c516ef0043b45027
Hongjun Yun <andylang8445@gmail.com>
2019-03-02 01:24:49 -0500

    Initial Commit

---
+++ Sudoku Answer checking program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku Answer checking program
+//
+//  Created by HongjunYun on 2019-03-02.
+//  Copyright © 2019 HongjunYun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
