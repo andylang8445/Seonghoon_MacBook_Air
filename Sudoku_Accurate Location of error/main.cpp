d9f5322c61e1a4bd7b69139e58d5c06338e7a5fa
Hongjun Yun <andylang8445@gmail.com>
2019-03-02 19:40:46 -0500

    Initial Commit

---
+++ Sudoku_Accurate Location of error/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku_Accurate Location of error
+//
+//  Created by HongjunYun on 2019-03-02.
+//  Copyright © 2019 HongjunYun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
